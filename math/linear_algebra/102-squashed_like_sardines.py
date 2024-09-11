53cb43ac6cb53a954743c6f9cabbe2c4734f4727
g-tumwesigye <g.tumwesigy@alustudent.com>
2024-09-11 17:51:34 +0200

    squahed

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,18 +1,23 @@
+#!/usr/bin/env python3
+"""
+Squashed Like Sardines
+"""
+
+
+import numpy as np
+
+
 def cat_matrices(mat1, mat2, axis=0):
-    # Validating shape compatibility (axis 0)
-    if axis == 0:
-        # same number of columns
-        if len(mat1[0]) != len(mat2[0]):
-            return None
-        # row axis
-        return [row for row in mat1] + [row for row in mat2]
+    """
+    Concatenates two matrices along a specific axis.
+    Returns None if the matrices cannot be concatenated.
+    """
+    mat1 = np.array(mat1)
+    mat2 = np.array(mat2)
     
-    # Validating shape compatibility (axis 1)
-    elif axis == 1:
-        # same number of rows
-        if len(mat1) != len(mat2):
-            return None
-        # Concatenate along the column axis
-        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
-    
-   
+    try:
+        result = np.concatenate((mat1, mat2), axis=axis)
+        return result.tolist()
+    except ValueError:
+        return None
+
